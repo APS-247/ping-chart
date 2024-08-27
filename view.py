9e200ce81eb600859070c267790b2edf4d02ce04
APS247 <apsinbox1@gmail.com>
2024-08-27 20:11:11 +0100

    No clue

--- view.py
+++ view.py
@@ -1,5 +1,6 @@
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+import matplotlib.animation as animation
 from ping_logger import ping_logger
 from tkinter import *
 
@@ -20,7 +21,6 @@ class Application(Frame):
         # The figure that contains the plot
         self.fig = Figure(figsize=(5, 3),
                           dpi=100)
-
         # The plot itself
         self.ax = self.fig.add_subplot()
         self.ax.set_ylim(0, self.PP.max_ping_axis)
@@ -33,19 +33,30 @@ class Application(Frame):
         self.canvas.draw()
 
         # Places the canvas on the Tkinter window
-        self.canvas.get_tk_widget().pack()
+        self.canvas.get_tk_widget().place(relx=0.5, rely=0, relheight=0.8, relwidth=1, anchor='n')
+
+        Frame1 = Frame(self.master, bd=5, bg=self.background_color)
+        Frame1.place(relx=0.5, rely=0.8, relheight=0.2, relwidth=1, anchor='n')
+
+        self.averageping = Label(Frame1, text="Ave:"+str(self.PP.ping_average))
+        self.averageping.place(relx=0, rely=0.5, relheight=0.4, relwidth=0.5)
+
+        self.highestping = Label(Frame1, text="Max:"+str(self.PP.ping_highest))
+        self.highestping.place(relx=0.55, rely=0.5, relheight=0.4, relwidth=0.5)
 
         # Starts the update loop for actually updating the graph over time
-        self.after(self.delay, self.update_graph, True)
+        self.anim = animation.FuncAnimation(self.fig, self.update_graph, interval=self.delay)
 
-    def update_graph(self, recurse=False):
+    def update_graph(self, i):
         self.ax.clear()
         self.ax.set_ylim(0, self.PP.max_ping_axis)
         self.ax.set_xlim(0, self.PP.max_ping_results)
+        self.ax.set_ylabel('Ping (ms)')
         self.ax.plot(self.PP.ping_results)
-        self.canvas.draw()
-        if recurse:
-            self.after(self.delay, self.update_graph, True)
+        # self.ax.
+        self.averageping.config(text="Ave:"+str(self.PP.ping_average))
+        self.highestping.config(text="Max:"+str(self.PP.ping_highest))
+        # self.canvas.draw()
 
     def on_closing(self):
         self.PP.stop_pinging()
